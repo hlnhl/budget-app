386ea505da316eeb98ee5e4a921df29d6a98eb0f
hlnhl <hoelhelen@gmail.com>
2023-10-23 11:06:40 -0700

    building logic for all components

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,30 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+        
+        /*const increaseAllocation = (name) => {
+            const expense = {
+                name: name,
+                cost: 10,
+            };
+    
+            dispatch({
+                type: 'ADD_EXPENSE',
+                payload: expense
+            });
+        }*/
+    
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+</div>
+    );
+};
+export default Budget;
